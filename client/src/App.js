514ff862ffccdbf54397593025c7abc3ec96dc26
Luv Gangwani <luvgangwani>
2021-09-13 02:23:21 +1000

    Post messages using a click event

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React , { useEffect } from 'react';
+import React , { Component } from 'react';
 import ws from 'websocket';
 
 const client = new ws.w3cwebsocket('ws://localhost:5000/', 'echo-protocol');
 
-function App() {
-  useEffect(() => {
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      message: '', // set the current message state
+      messages: [], // list of all messages
+    };
+    this.handleSendClick = this.handleSendClick.bind(this);
+  }
+
+  componentDidMount() {
     // set up the connect event listener
     client.onopen = () => {
       console.log('WebSocket client connected');
-
-      // send a message
-      if (client.readyState === client.OPEN) {
-        client.send(Math.random().toString());
-      }
     }
 
     // set up the event listener for when the connection is closed
@@ -22,8 +27,9 @@ function App() {
 
     // set up the event listener for when a message is received
     client.onmessage = (message) => {
+      const { messages } = this.state;
       if (typeof message.data === 'string') {
-        console.log(`${new Date()}: Received message by the client: "${message.data}"`);
+        this.setState({ messages: [`${new Date()}: ${message.data}`, ...messages] });
       }
     };
 
@@ -31,12 +37,34 @@ function App() {
     client.onerror = (error) => {
       console.log(`Connection error: ${error.toString()}`);
     }
+  }
 
-  }, []);
+  // handler function for sending a message
+  handleSendClick() {
+    const { message } = this.state;
+    // send a message
+    if (client.readyState === client.OPEN) {
+      client.send(message);
+    }
+  }
 
-  return (
-   <div></div>
-  );
+  render() {
+    const { message, messages } = this.state;
+    return (
+      <div>
+        <div>
+         <h1>Messages</h1>
+         {
+           messages.map((eachMessage, index) => (
+             <div key={index}>{eachMessage}</div>
+           ))
+         }
+        </div>
+        <input type="text" placeholder="Message" onChange={(e) => this.setState({ message: e.target.value })} value={message} />
+        <button onClick={this.handleSendClick}>Send</button>
+      </div>
+     );
+  }
 }
 
 export default App;
